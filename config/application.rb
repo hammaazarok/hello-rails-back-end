80232e780a7deb224baf3a4d12c3e5c21b682145
Hamma Azarok <hammaagazarok@gmail.com>
2022-11-22 23:47:45 +0200

    setup rails cors

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative "boot"
-
+require 'rack'
+require 'rack/cors'
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,6 +11,12 @@ module HelloRailsBackEnd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '/*', :headers => :any, :methods => :patch
+      end
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
